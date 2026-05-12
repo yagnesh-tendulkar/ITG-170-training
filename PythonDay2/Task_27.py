403618c42f058be9a7f08f2db023c910b9ae0748
Bikash Ranjan Barik <156284620+itz-bikash20@users.noreply.github.com>
2026-05-12 10:53:05 +0530

    Add files via upload

---
+++ PythonDay2/Task_27.py
@@ -0,0 +1,9 @@
+arr3=[]
+r = int(input("Enter the range of the array: "))
+for i in range(1,r+1):
+    n=int(input(f"Enter the {i}th element: "))
+    arr3.append(n)
+print("arr3: ", arr3)
+
+arr1=arr3
+print("arr1: ", arr1)
