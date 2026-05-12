eb6be2c31348f15d170a44e1754ba7e55a08d469
Bikash Ranjan Barik <156284620+itz-bikash20@users.noreply.github.com>
2026-05-12 10:41:36 +0530

    Add files via upload

---
+++ PythonDay2/Task_25.py
@@ -0,0 +1,11 @@
+for num in range(1, 1001):
+    temp = num
+    sum = 0
+
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** 3
+        temp = temp // 10
+
+    if sum == num:
+        print(num, end=" ")
