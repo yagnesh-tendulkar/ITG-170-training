395004363b4f15dce0031d0539c6608b3635781c
Amrita Pattanayak <amritapattanayak04@gmail.com>
2026-05-25 11:27:11 +0530

    Delete custom_exception.py

--- custom_exception.py
+++
@@ -1,8 +0,0 @@
-class HRException(Exception):
-    pass
-class EmployeeException(Exception):
-    pass
-class NotFoundException(Exception):
-    pass
-class AuthenticationException(Exception):
-    pass
