b3fcb07c743ca79161749adaa66f9fd18680c7e5
Abhiram12331 <151455564+Abhiram12331@users.noreply.github.com>
2026-05-27 23:14:58 +0530

    Delete 21th-May-FastApi/EmployeeManagementSystem directory

--- 21th-May-FastApi/EmployeeManagementSystem/auth/Logout.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.post("/logout")
-def logout():
-    # Since login is stateless, "logging out" just means
-    # the client should delete their stored info (e.g., localStorage)
-    return {"message": "Logged out successfully"}
