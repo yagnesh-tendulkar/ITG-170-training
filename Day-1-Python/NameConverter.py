49f0aa89b10cb34742a59c5e9ac7e913c752f4f4
Abhiram12331 <151455564+Abhiram12331@users.noreply.github.com>
2026-05-27 23:17:25 +0530

    Delete Day-1-Python directory

--- Day-1-Python/NameConverter.py
+++
@@ -1,4 +0,0 @@
-name=input("Enter your name: ")
-print("Your name in uppercase:",name.upper())
-print("Your name in lowercase:",name.lower())
-print("Name\'s length is ",len(name))
--- Day-1-Python/StringOperation.py
+++
@@ -1,6 +0,0 @@
-single='Hello World!'
-double="Hello World!"
-triple="""Hello World!"""
-print(single)
-print(double)
-print(triple)
