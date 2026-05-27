33daa07573ba0bcfabd02059f02334a1ea088912
Abhiram12331 <151455564+Abhiram12331@users.noreply.github.com>
2026-05-27 23:18:46 +0530

    Delete Day-4-Python directory

--- Day-4-Python/Day-4 tasks/Area.py
+++
@@ -1,7 +0,0 @@
-def area(l,b):
-    return l*b
-
-l= int(input('Enter the length of the rectangle : '))
-b = int(input('Enter the breadth of the rectangle : '))
-
-print('Area of rectangle: ',area(l,b))
--- Day-4-Python/DictionaryComprehension.py
+++
@@ -1,5 +0,0 @@
-squares={x:x*x for x in range(1,11) if x%2==0}
-# print(squares)
-
-dict={100:'Alex',200:'John',300:'Michael',400:'Smith'}
-
