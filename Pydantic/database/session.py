99b38d9d0c6b7dd7140c1d1e4bf05585f2cde105
arikaneemadarshita <arikaneemadarshita@gmail.com>
2026-05-28 14:36:15 +0530

    Add files via upload

---
+++ Pydantic/database/session.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import sessionmaker
+
+from Pydantic.database.database import engine
+
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
