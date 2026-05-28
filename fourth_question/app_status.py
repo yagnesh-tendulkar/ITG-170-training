d87b8e5d4ac97ef4267f74295c26cbfb4f174b99
Geetha Gangabattula <142496156+Geethaganbattula@users.noreply.github.com>
2026-05-28 10:52:19 +0530

    Create app_status.py

---
+++ fourth_question/app_status.py
@@ -0,0 +1,15 @@
+class AppStatus:
+
+    # SUCCESS
+    HTTP_200_OK = 200
+    HTTP_201_CREATED = 201
+
+    # ERRORS
+    HTTP_404_NOT_FOUND = 404
+    HTTP_500_INTERNAL_SERVER_ERROR = 500
+
+    # MESSAGES
+    ICECREAM_CREATED = "Ice Cream Added Successfully"
+    ICECREAM_UPDATED = "Ice Cream Updated Successfully"
+    ICECREAM_DELETED = "Ice Cream Deleted Successfully"
+    ICECREAM_NOT_FOUND = "Ice Cream Not Found"
