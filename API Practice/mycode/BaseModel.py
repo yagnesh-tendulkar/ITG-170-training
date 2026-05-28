8bece099781a4eb1b4c438acc61996babfefe929
BALAJI PASAM <balajipasam106@gmail.com>
2026-05-28 15:07:06 +0530

    Add User model with Pydantic validation
    Created a User model using Pydantic's BaseModel for validation.

---
+++ API Practice/mycode/BaseModel.py
@@ -0,0 +1,14 @@
+#usage of pydantic basemodel class
+from pydantic import BaseModel,ValidationError
+
+
+class User(BaseModel):
+"""Creating a data model class with name User ,where this class represents the request data model/blueprint"""
+  id :int
+  name:str
+  is_active:bool=True
+
+#validation
+valid_user={"id":"1","name":"balaji"}
+user=User(**valid_user)
+print(user.name)
