daa61c0eb0c54a2613d85c0d65b58a7ceba6bab6
avalaprabhash <prabhasavala7@gmail.com>
2026-06-01 14:44:12 +0530

    Delete app directory

--- app/middleware/CustomMiddleware.py
+++
@@ -1,9 +0,0 @@
-from fastapi import Request
-
-async def log_requests(request : Request, call_next):
-    print(request.url)
-    print(request.method)
-    response = await call_next(request)
-
-    return response
-
