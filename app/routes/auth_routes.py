1f92bd7aee830123bbdc2bb5fd086cb1ce5effed
vaishnavi4910 <162040412+vaishnavi4910@users.noreply.github.com>
2026-06-01 17:55:07 +0530

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -3,7 +3,6 @@ from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
