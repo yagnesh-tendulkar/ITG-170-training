257a3bce2e09d43492ffd0302094bd00cacdba3f
Rudraprasad Tripathy <rtripathy@miraclesoft.com>
2026-06-01 16:40:05 +0530

    compltete project

--- PythonProject/FILE UPLOAD AND TASK MANAGEMENT SYSTEM/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from app.models.user import User
-from app.models.api_key import ApiKey
-from app.models.task import Task
-from app.models.file import File
-from app.models.job import Job
--- PythonProject/FILE UPLOAD AND TASK MANAGEMENT SYSTEM/app/models/init.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .task import Task
-from .file import File
-from .api_key import ApiKey
-from .job import Job
