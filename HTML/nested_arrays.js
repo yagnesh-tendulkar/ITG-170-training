e4b01dad3c6e817f731a88fb5ef8aa970aa90ad4
BALAJI PASAM <balajipasam106@gmail.com>
2026-06-10 14:41:13 +0530

    Add nested_arrays.js with plant and animal lists

---
+++ HTML/nested_arrays.js
@@ -0,0 +1,13 @@
+
+//nested arrays
+var myPlants=[
+    {
+        type:"flowers",
+        list:["rose","tulip","lilly"]
+    },
+    {
+        type:"animals",
+        list:["dog","cat","tiger"]
+    }
+];
+console.log(myPlants[0].list[2]);
