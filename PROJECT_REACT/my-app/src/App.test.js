d6ff739b9b57f97187fa241262b6b7255aa01263
BALAJI PASAM <balajipasam106@gmail.com>
2026-06-18 06:52:11 +0000

    PROJECT COUNTER using react

---
+++ PROJECT_REACT/my-app/src/App.test.js
@@ -0,0 +1,13 @@
+function App() {
+    const stname="Balaji";
+    const stCourse="REACT";
+  return (
+    <div>
+      <h1>React application</h1>
+      {/* <Student name={stname} course={stCourse}/> */}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
